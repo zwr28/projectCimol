ed25918c1aa4b3d1125559065d2d32c973c71bd8
zwr28 <71338093+zwr28@users.noreply.github.com>
2021-03-03 17:42:46 +0700

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -78,7 +78,7 @@ let questions = [
 const lastQuestion = questions.length - 1;
 let runningQuestion = 0;
 let count = 0;
-const questionTime = 1000; // 10s
+const questionTime = 10; // 10s
 const gaugeWidth = 150; // 150px
 const gaugeUnit = gaugeWidth / questionTime;
 let TIMER;
